5c9a0d2a60d14fd80f3d4e5aca2eefa7458b8d35
JonathanOliveros <35575171+JonathanOliveros@users.noreply.github.com>
2019-10-02 18:50:32 -0600

    Add files via upload

--- assignment05/QuickSortPivotRandom.java
+++ assignment05/QuickSortPivotRandom.java
@@ -1,6 +1,7 @@
 package assignment05;
 
 import java.util.List;
+import java.util.Random;
 
 /**
  * Implements in-place QuickSort using the {@code Sorter} interface by selecting
@@ -8,26 +9,28 @@ import java.util.List;
  * 
  * This class must implement the {@code setThreshold, threshold} methods.
  * 
- * @author Joshua Wells and Jonathan Oliveros
+ * @author Josh Wells, Jonathan Oliveros
  *
  * @param <T> type of the element of the list this sorter can sort
  */
-public class QuickSortPivotRandom<T extends Comparable<? super T>>
-    extends AbstractQuickSort<T> {
+public class QuickSortPivotRandom<T extends Comparable<? super T>> extends AbstractQuickSort<T> {
 
-  public QuickSortPivotRandom() {
-	  	this.name = "QuickSortPivotRandom";
-	    this.complexity = ComplexityClass.NLOGN;
-	    this.setThreshold(40); //default threshold
-}
+	public QuickSortPivotRandom() {
+		this.name = "QuickSortPivotRandom";
+		this.complexity = ComplexityClass.NLOGN;
+		this.setThreshold(5); // default threshold
+	}
 
-  @Override
-  protected T pivot(List<T> list, int start, int end) {
-    assert list != null : "Violation of: list is not null";
-    return null; // FIXME
-  }
+	@Override
+	protected T pivot(List<T> list, int start, int end) {
+		assert list != null : "Violation of: list is not null";
+		Random r = new Random();
+		int position = r.nextInt(list.size());
+		SortUtils.swapElementsAt(list, start, position);
+		return list.get(start);
+	}
 
-  // TODO Override other methods if required
-  // TODO Add private helper methods as needed
+	// TODO Override other methods if required
+	// TODO Add private helper methods as needed
 
 }
